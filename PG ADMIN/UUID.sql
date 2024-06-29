d37f5c9199128fb8c19aa9ccc3063c543268549e
sathish kumar <skch2710@gmail.com>
2024-06-29 11:10:19 +0530

    Added Interval

--- PG ADMIN/UUID.sql
+++ PG ADMIN/UUID.sql
@@ -8,3 +8,10 @@ SELECT (EXTRACT(EPOCH FROM NOW()) * 1000)::BIGINT AS current_time_milliseconds;
 
 select CONCAT(uuid_generate_v4(),'#',(EXTRACT(EPOCH FROM NOW()) * 1000)::BIGINT)
 
+
+SELECT now() + INTERVAL '1 day';
+
+
+SELECT now() + INTERVAL '24 hours';
+
+
