79290f2f1309c7e016071a85ecba6a1406f5e1df
sathish kumar <skch2710@gmail.com>
2025-01-01 18:47:20 +0530

    Update SQLQuery.sql

--- MS SQL/SQLQuery.sql
+++ MS SQL/SQLQuery.sql
@@ -13,5 +13,29 @@ SELECT CONCAT('abc',' - ','cdf');
 
 SELECT 1;
 
+--- DATE FORMAT
 
+SELECT FORMAT(GETDATE(), 'MM/dd/yyyy');
+
+SELECT CAST(FORMAT(GETDATE(), 'MM/dd/yyyy') AS VARCHAR(10));
+
+SELECT CONVERT(VARCHAR, GETDATE(), 101);
+
+SELECT CAST(GETDATE() AS DATE);
+
+--- JSON
+
+SELECT * FROM [hostel].[users]
+
+SELECT 
+    user_id AS user_id,
+    email_id AS email_id
+FROM [hostel].[users]
+FOR JSON PATH, WITHOUT_ARRAY_WRAPPER;
+
+SELECT 
+    MAX(dob) AS max_dob,
+    MIN(dob) AS min_dob
+FROM [hostel].[users] 
+FOR JSON PATH, WITHOUT_ARRAY_WRAPPER;
 
